7371d534bbcfd30c2741a69b416ee798f9e5d1fa
MOHIT AWASTHI <119238282+mohit567233@users.noreply.github.com>
2024-06-05 21:57:03 +0530

    variables

---
+++ ex_03062024/lab003.py
@@ -0,0 +1,10 @@
+print("my hole world")
+age=65
+name="mohit"
+ismale=True
+pi=3.2
+#How do i check the types of variables?
+print(type(name))
+print(type(age))
+print(type(ismale))
+print(type(pi))
